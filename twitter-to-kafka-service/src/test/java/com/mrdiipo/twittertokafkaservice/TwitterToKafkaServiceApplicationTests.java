83d8b603ed2d4cd022787ba32452456afcd91abf
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-03-14 09:45:29 +0100

    initialize twitter-to-kafka-service

---
+++ twitter-to-kafka-service/src/test/java/com/mrdiipo/twittertokafkaservice/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.twittertokafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
