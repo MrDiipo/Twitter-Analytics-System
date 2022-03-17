b2b911e8659ab35afa2761f02b433208e226e393
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-03-17 18:24:02 +0100

    Add configuration data service

---
+++ app-config-data/src/main/java/com/mrdiipo/appconfigdata/AppConfigDataApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.appconfigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfigDataApplication.class, args);
+	}
+
+}
