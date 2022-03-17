53809fffca48987ab7849d915029ff1996716dfb
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-03-17 18:23:34 +0100

    Add configuration data service

---
+++ app-config-data/src/test/java/com/mrdiipo/appconfigdata/AppConfigDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.appconfigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConfigDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
