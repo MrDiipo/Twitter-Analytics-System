259b33d623012a90693973dacbe8403bce06ab6b
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-05-18 17:03:31 +0100

    Develop kafka admin module

---
+++ common-config/src/main/java/com/mrdiipo/commonconfig/CommonConfigApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.commonconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonConfigApplication.class, args);
+	}
+
+}
---
+++ common-config/src/test/java/com/mrdiipo/commonconfig/CommonConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.commonconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
